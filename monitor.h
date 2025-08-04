28e99235c1f2d47e9bd7cb9db494f04766ea7d45
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-04 12:10:05 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,14 @@
 #pragma once
+#include <string>
+#include <functional>
+
+struct vitalInfoandRange {
+    std::string vitalType;
+    float value;
+    float min;
+    float max;
+};
+
+bool vitalRangechekandAlertmsg(vitalInfoandRange& vitaldetails, std::function<void(std::string&)> prntWrng);
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
