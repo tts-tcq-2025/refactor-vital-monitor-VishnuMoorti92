0b5fbdaa86d8e57cb89c4cf841bdde449978389a
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-05 09:11:34 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -16,10 +16,7 @@ void dynamicUpdateandWarningandPrint(const std::string& message) {
 }
 
 bool vitalRangechek(vitalInfoandRange& vitaldetails) {
-    if(vitaldetails.vitalType != "Oxygen Saturation") {
-        return(vitaldetails.value < vitaldetails.min || vitaldetails.value > vitaldetails.max);
-    }
-    return(vitaldetails.value < vitaldetails.min);
+    return(vitaldetails.value < vitaldetails.min || vitaldetails.value > vitaldetails.max);
 }
 
 bool vitalRangeAlertmsg(vitalInfoandRange& vitaldetails, std::function<void(std::string&)> prntWrng){
@@ -38,7 +35,7 @@ void printalertmassage(std::string& lineContent){
 int vitalsOk(float temperature, float pulseRate, float spo2, std::function<void(std::string&)> prntWrng) {
 vitalInfoandRange vitaldetails[] = {{"Temperature", temperature, 95.0, 102.0},
                                     {"Pulse Rate", pulseRate, 60.0, 100.0},
-                                    {"Oxygen Saturation", spo2, 90.0, 89.0}};
+                                    {"Oxygen Saturation", spo2, 90.0, 100.0}};
 
     bool vitalsChkOk = true;
     for (int i = 0; i < 3; ++i) {
