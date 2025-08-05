4be58d3f253320813dff5ec8ef4e2f10a81479b6
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-05 09:57:50 +0530

    Update monitor.hpp

--- monitor.hpp
+++ monitor.hpp
@@ -2,16 +2,29 @@
 #include <string>
 #include <functional>
 
-struct vitalInfoandRange {
+// Class definition replacing the struct
+class VitalInfoAndRange {
+private:
     std::string vitalType;
     float value;
     float min;
     float max;
+
+public:
+    VitalInfoAndRange(const std::string& type, float val, float minVal, float maxVal)
+        : vitalType(type), value(val), min(minVal), max(maxVal) {}
+
+    std::string getVitalType() const { return vitalType; }
+    float getValue() const { return value; }
+    float getMin() const { return min; }
+    float getMax() const { return max; }
+
+    void setValue(float val) { value = val; }
+    void setMin(float minVal) { min = minVal; }
+    void setMax(float maxVal) { max = maxVal; }
 };
 
-bool vitalRangechek(vitalInfoandRange& vitaldetails);
-bool vitalRangeAlertmsg(vitalInfoandRange& vitaldetails, std::function<void(std::string&)> prntWrng);
-
+// Function declarations
+bool vitalRangeCheck(VitalInfoAndRange& vitalDetails);
+bool vitalRangeAlertMsg(VitalInfoAndRange& vitalDetails, std::function<void(std::string&)> prntWrng);
 int vitalsOk(float temperature, float pulseRate, float spo2, std::function<void(std::string&)> prntWrng);
-
-
