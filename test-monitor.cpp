377d0fb4b379bd85205a5085ce78731f05c8080a
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-05 09:06:23 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -9,12 +9,13 @@ auto alertfunc = [&messages](const std::string& msg) {
     };
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_TRUE(vitalsOk(94, 80, 95, alertfunc));
-  ASSERT_FALSE(vitalsOk(100, 70, 98, alertfunc));
-  ASSERT_TRUE(vitalsOk(105, 80, 95, alertfunc));
-  ASSERT_TRUE(vitalsOk(100, 59, 95, alertfunc));
-  ASSERT_TRUE(vitalsOk(100, 101, 95, alertfunc));
-  ASSERT_TRUE(vitalsOk(100, 75, 88, alertfunc));
+  ASSERT_FALSE(vitalsOk(94, 80, 95, alertfunc));
+  ASSERT_TRUE(vitalsOk(100, 70, 98, alertfunc));
+  ASSERT_FALSE(vitalsOk(105, 80, 95, alertfunc));
+  ASSERT_FALSE(vitalsOk(100, 59, 95, alertfunc));
+  ASSERT_FALSE(vitalsOk(100, 101, 95, alertfunc));
+  ASSERT_FALSE(vitalsOk(100, 75, 88, alertfunc));
 }
+
 
 
