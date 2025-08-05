439098ab88bfb76ad9311f18cd39685836dce72a
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-05 09:19:03 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -12,10 +12,11 @@ TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(94, 80, 95, alertfunc));
   ASSERT_TRUE(vitalsOk(100, 70, 98, alertfunc));
   ASSERT_FALSE(vitalsOk(105, 80, 95, alertfunc));
-  ASSERT_FALSE(vitalsOk(100, 55, 95, alertfunc));
+  ASSERT_TRUE(vitalsOk(100, 55, 95, alertfunc));
   ASSERT_FALSE(vitalsOk(100, 101, 95, alertfunc));
   ASSERT_FALSE(vitalsOk(100, 75, 88, alertfunc));
 }
+
 
 
 
