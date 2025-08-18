15c3c01bfb8fc8de99382f26c7f86e5a58cb40ad
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-18 23:46:10 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,23 +3,44 @@
 #include <string>
 #include "monitor.h"
 
-std::vector<std::string> messages;
-auto alertfunc = & {
-    messages.push_back(msg);
-};
-
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
     messages.clear();
-    ASSERT_FALSE(vitalsOk(94, 80, 95, alertfunc));  // Temperature too low
+    ASSERT_FALSE(vitalsOk(94, 80, 95, alertfunc));
     messages.clear();
-    ASSERT_TRUE(vitalsOk(100, 70, 98, alertfunc));  // All vitals OK
+    ASSERT_TRUE(vitalsOk(100, 70, 98, alertfunc));
     messages.clear();
-    ASSERT_FALSE(vitalsOk(105, 80, 95, alertfunc)); // Temperature too high
+    ASSERT_FALSE(vitalsOk(105, 80, 95, alertfunc));
     messages.clear();
-    ASSERT_FALSE(vitalsOk(100, 58, 95, alertfunc)); // Pulse Rate too low
+    ASSERT_FALSE(vitalsOk(100, 58, 95, alertfunc));
     messages.clear();
-    ASSERT_FALSE(vitalsOk(100, 101, 95, alertfunc));// Pulse Rate too high
+    ASSERT_FALSE(vitalsOk(100, 101, 95, alertfunc));
     messages.clear();
-    ASSERT_FALSE(vitalsOk(100, 75, 89.9, alertfunc));// SPO2 too low
+    ASSERT_FALSE(vitalsOk(100, 75, 89.9, alertfunc));
 }
+
+TEST(Monitor, WarningGeneratedNearLowerThreshold) {
+    messages.clear();
+    ASSERT_TRUE(vitalsOk(95.1, 60.1, 90.1, alertfunc));
+    ASSERT_FALSE(messages.empty());
+    ASSERT_NE(std::find_if(messages.begin(), messages.end(),  {
+        return msg.find("approaching lower threshold") != std::string::npos;
+    }), messages.end());
+}
+
+TEST(Monitor, WarningGeneratedNearUpperThreshold) {
+    messages.clear();
+    ASSERT_TRUE(vitalsOk(101.1, 99.1, 99.1, alertfunc));
+    ASSERT_FALSE(messages.empty());
+    ASSERT_NE(std::find_if(messages.begin(), messages.end(),  {
+        return msg.find("approaching upper threshold") != std::string::npos;
+    }), messages.end());
+}
+
+TEST(Monitor, NoWarningOrAlertWhenVitalsAreMidRange) {
+    messages.clear();
+    ASSERT_TRUE(vitalsOk(98.6, 75, 96, alertfunc));
+    ASSERT_TRUE(messages.empty());
+}
+
+
 
