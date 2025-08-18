3fec679e7f2ef4677fab6b3870695e65114a3b48
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-18 23:35:30 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -4,24 +4,22 @@
 #include "monitor.h"
 
 std::vector<std::string> messages;
-auto alertfunc = [&messages](const std::string& msg) {
-        messages.push_back(msg);
-    };
+auto alertfunc = & {
+    messages.push_back(msg);
+};
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(94, 80, 95, alertfunc));
-  ASSERT_TRUE(vitalsOk(100, 70, 98, alertfunc));
-  ASSERT_FALSE(vitalsOk(105, 80, 95, alertfunc));
-  ASSERT_FALSE(vitalsOk(100, 58, 95, alertfunc));
-  ASSERT_FALSE(vitalsOk(100, 101, 95, alertfunc));
-  ASSERT_FALSE(vitalsOk(100, 75, 89.9, alertfunc));
+    messages.clear();
+    ASSERT_FALSE(vitalsOk(94, 80, 95, alertfunc));  // Temperature too low
+    messages.clear();
+    ASSERT_TRUE(vitalsOk(100, 70, 98, alertfunc));  // All vitals OK
+    messages.clear();
+    ASSERT_FALSE(vitalsOk(105, 80, 95, alertfunc)); // Temperature too high
+    messages.clear();
+    ASSERT_FALSE(vitalsOk(100, 58, 95, alertfunc)); // Pulse Rate too low
+    messages.clear();
+    ASSERT_FALSE(vitalsOk(100, 101, 95, alertfunc));// Pulse Rate too high
+    messages.clear();
+    ASSERT_FALSE(vitalsOk(100, 75, 89.9, alertfunc));// SPO2 too low
 }
-
-
-
-
-
-
-
-
 
