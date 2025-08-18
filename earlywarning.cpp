f13767d052934b62e8844b1f6c36f4e2c7242a66
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-18 16:18:50 +0530

    Update earlywarning.cpp

--- earlywarning.cpp
+++ earlywarning.cpp
@@ -1,22 +1,40 @@
 #include "earlywarning.hpp"
 
+namespace {
+    float calculateTolerance(const vitalInfoandRange& vital) {
+        return 0.015f * vital.max;
+    }
+
+    bool isApproachingLowerBound(const vitalInfoandRange& vital, float tolerance) {
+        return vital.value >= vital.min && vital.value < vital.min + tolerance;
+    }
+
+    bool isApproachingUpperBound(const vitalInfoandRange& vital, float tolerance) {
+        return vital.value <= vital.max && vital.value > vital.max - tolerance;
+    }
+}
+
 bool EarlyWarning::isInWarningRange(const vitalInfoandRange& vital, std::string& warningMsg) {
-    float tolerance = 0.015f * vital.max;
+    float tolerance = calculateTolerance(vital);
 
     if (vital.vitalType == "Oxygen Saturation") {
-        if (vital.value >= vital.min && vital.value < vital.min + tolerance) {
+        if (isApproachingLowerBound(vital, tolerance)) {
             warningMsg = "Warning: Approaching hypoxia";
             return true;
         }
-    } else {
-        if (vital.value >= vital.min && vital.value < vital.min + tolerance) {
-            warningMsg = "Warning: Approaching hypothermia";
-            return true;
-        }
-        if (vital.value <= vital.max && vital.value > vital.max - tolerance) {
-            warningMsg = "Warning: Approaching hyperthermia";
-            return true;
-        }
+        return false;
     }
+
+    if (isApproachingLowerBound(vital, tolerance)) {
+        warningMsg = "Warning: Approaching hypothermia";
+        return true;
+    }
+
+    if (isApproachingUpperBound(vital, tolerance)) {
+        warningMsg = "Warning: Approaching hyperthermia";
+        return true;
+    }
+
     return false;
 }
+
