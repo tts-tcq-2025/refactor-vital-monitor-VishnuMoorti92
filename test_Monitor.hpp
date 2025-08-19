223d8ade6692978d6850a217c93e666f56c79773
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-19 09:48:53 +0530

    Update test_Monitor.hpp

--- test_Monitor.hpp
+++ test_Monitor.hpp
@@ -1,19 +1,8 @@
-#include <gtest/gtest.h>
-#include <vector>
-#include <string>
-#include <functional>
+#pragma once
 
-namespace EarlyWarning {
-    bool isInWarningRange(const vitalInfoandRange& vital, std::string& warningMsg) {
-        // Simulate warning if value is within 1 unit of min or max
-        if (vital.value >= vital.min && vital.value <= vital.min + 1) {
-            warningMsg = vital.vitalType + " is approaching lower threshold!";
-            return true;
-        }
-        if (vital.value <= vital.max && vital.value >= vital.max - 1) {
-            warningMsg = vital.vitalType + " is approaching upper threshold!";
-            return true;
-        }
-        return false;
-    }
-}
+#include <string>
+#include <vector>
+
+// A helper function to simulate alert collection
+extern std::vector<std::string> messages;
+extern std::function<void(const std::string&)> alertfunc;
